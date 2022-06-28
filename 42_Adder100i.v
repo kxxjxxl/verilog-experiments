c974af4a96b252091ea497dcfcc60542ab7320c0
Emad Salim <62475696+kxxjxxl@users.noreply.github.com>
2022-06-27 22:52:20 -0230

    Update 42_Adder100i.v

--- 42_Adder100i.v
+++ 42_Adder100i.v
@@ -6,8 +6,8 @@ module top_module(
     
     always @(*) begin
         for(int i = 0; i < 100; i++) begin
-            if(!i) begin  // 加法刚开始，所以进位标志用cin
-                if(!cin) begin // 第一种情况，无进位
+            if(!i) begin  
+                if(!cin) begin 
                     if(a[i] == 0 && b[i] == 0) begin
                         sum[i] = 0;
                         cout[i] = 0;
@@ -51,7 +51,7 @@ module top_module(
                         cout[i] = 1;
                     end
                 end
-                else begin // 第二种情况，进位
+                else begin 
                     if(a[i] == 0 && b[i] == 0) begin
                         sum[i] = 1;
                         cout[i] = 0;
@@ -69,7 +69,7 @@ module top_module(
         end
     end
 
-    // M-HHH 提供的精简思路
+    
     // assign cout[0] = a[0] & b[0] | a[0] & cin | b[0] & cin;
     // assign sum[0]  = a[0] ^ b[0] ^ cin;
     
@@ -91,7 +91,7 @@ module top_module(
     //             end
     //     end
 
-    //基于 M-HHH 的思路作出的二次优化
+   
     // assign cout[0] = a[0] & b[0] | a[0] & cin | b[0] & cin;
     // assign sum[0]  = a[0] ^ b[0] ^ cin;
     
